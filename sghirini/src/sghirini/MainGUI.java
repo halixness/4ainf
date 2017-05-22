5d0261f1be93fb4bfc97fc460082bb5e9db3a3a8
GHIRINI MARCO (sGHIRINI) <sGHIRINI>
2017-05-22 09:24:01 +0200

    Creata Interfaccia

---
+++ sghirini/src/sghirini/MainGUI.java
@@ -0,0 +1,78 @@
+package sghirini;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class MainGUI extends JFrame implements WindowListener, ActionListener{
+	private JButton b = new JButton("Apri log");
+	public MainGUI()
+	{
+		this.addWindowListener(this);
+		this.setSize(300, 350);
+		this.setLocationRelativeTo(null);
+		this.setBackground(Color.lightGray);
+		this.setLayout(new GridLayout(19, 1));
+	}
+
+	@Override
+	public void windowActivated(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosed(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosing(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowIconified(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowOpened(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		new MainGUI();
+	}
+}
