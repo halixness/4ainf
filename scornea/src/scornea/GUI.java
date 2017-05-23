426f31c4143f9df0233cc7031d137123c5429ee5
SCAURI CORNEA MARIA (sCORNEA) <sCORNEA>
2017-05-23 08:20:05 +0200

    maria1

--- scornea/src/scornea/GUI.java
+++ scornea/src/scornea/GUI.java
@@ -1,16 +1,28 @@
 package scornea;
 
 import java.awt.BorderLayout;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class GUI extends JFrame {
 
 	private JPanel contentPane;
+    private File f;
 
+    
+	
+	
+	
 	/**
 	 * Launch the application.
 	 */
@@ -30,13 +42,29 @@ public class GUI extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public GUI() {
+	public GUI() 
+	{
+		f = new File ("log.txt");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JButton btnSalvaFile = new JButton("SALVA FILE");
+		btnSalvaFile.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent arg0) 
+			{			
+				
+			}
+		});
+		contentPane.add(btnSalvaFile, BorderLayout.CENTER);
+		
+		
+		
+		
 	}
 
 }
