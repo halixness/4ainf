de49b66dfec44d729189afaede5babcc9c782ae2
GHIRINI MARCO (sGHIRINI) <sGHIRINI>
2017-05-29 08:32:36 +0200

    Commit finale

--- sghirini/src/sghirini/MainGUI.java
+++ sghirini/src/sghirini/MainGUI.java
@@ -6,22 +6,33 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTextPane;
 
 @SuppressWarnings("serial")
 public class MainGUI extends JFrame implements WindowListener, ActionListener{
 	private JButton b = new JButton("Apri log");
+	private JTextPane tx = new JTextPane();
+	
 	public MainGUI()
 	{
 		this.addWindowListener(this);
-		this.setSize(300, 350);
+		this.setSize(500, 200);
 		this.setLocationRelativeTo(null);
 		this.setBackground(Color.lightGray);
-		
+		this.setLayout(new GridLayout(2,1));
 		b.addActionListener(this);
-		this.add(b, "Center");
+		
+		this.add(tx);
+		this.add(b);
 		
 		this.setVisible(true);
 	}
@@ -71,9 +82,29 @@ public class MainGUI extends JFrame implements WindowListener, ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
+		String linea;
+		BufferedReader br = null;
+		ArrayList<String> record = new ArrayList<String>(3);
+		
 		if(arg0.getSource() == b)
 		{
+			try {
+				br = new BufferedReader(new FileReader(new File("log.txt")));
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}			
 			
+			try {
+				while ((linea = br.readLine()) != null)
+						record.add(linea);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			tx.setText(record.toString());
+		
 		}
 		
 	}
