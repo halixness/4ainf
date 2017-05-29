17f38776b1fd5111b2d1470ba3e73815fdb07d26
STRINA RICCARDO (sSTRINA3) <sSTRINA3>
2017-05-29 08:39:07 +0200

    modifico il src

--- sStrina3/src/code/sStrina3/GitGUI.java
+++ sStrina3/src/code/sStrina3/GitGUI.java
@@ -1,7 +1,6 @@
 package code.sStrina3;
 
 import java.awt.BorderLayout;
-import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
@@ -9,7 +8,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Vector;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -25,17 +23,19 @@ public class GitGUI extends JFrame implements ActionListener{
 
 	public GitGUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 600, 400);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		
-		alfa = new JButton("New button");
+		alfa = new JButton("Cliccami");
+		alfa.setToolTipText("Visualizza il file log.txt\r\n");
 		alfa.addActionListener(this);
 		contentPane.add(alfa, BorderLayout.SOUTH);
 		
 		beta = new JTextPane();
+		beta.setEditable(false);
 		contentPane.add(beta, BorderLayout.CENTER);
 		setVisible(true);
 	}
@@ -43,14 +43,12 @@ public class GitGUI extends JFrame implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		try {
-			BufferedReader rb = new BufferedReader(new FileReader(new File("src//code//sStrina3//log.txt")));
+			BufferedReader rb = new BufferedReader(new FileReader(new File("log.txt")));
 			Vector<String> log = new Vector<String>();
 			
 			while(rb.ready())
 			{
 				log.add(rb.readLine());
-				System.out.println("arabia");
-				System.out.println(log.get(0));
 			}
 			
 			beta.setText(log.toString());
@@ -60,7 +58,6 @@ public class GitGUI extends JFrame implements ActionListener{
 			rb.close();
 		} catch (IOException e) {
 			e.printStackTrace();
-			System.out.println("arabia");
 		}
 		
 	}
