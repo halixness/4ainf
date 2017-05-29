d2224acb9c0b144950dd85c01ef36ef9c2216214
Diego Calanzone <diegocalanzone@MacBook-Pro-di-Diego.local>
2017-05-29 11:04:21 +0200

    sCALANZO compleato

--- sCALANZO/src/sCALANZO/Main.java
+++ sCALANZO/src/sCALANZO/Main.java
@@ -2,10 +2,21 @@ package sCALANZO;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.EOFException;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 
 import javax.swing.*;
 
@@ -13,6 +24,9 @@ import java.net.*;
 
 
 public class Main extends JFrame implements ActionListener, KeyListener, WindowListener {
+	
+	JTextArea logContent;
+	JButton openLog;
 	
 	// Constructors
 	public Main() {
@@ -36,7 +50,31 @@ public class Main extends JFrame implements ActionListener, KeyListener, WindowL
 	    label.setLocation(0, 0);
 	    label.setSize(600,550);
 	    this.add(label);
+
+		/*
+		 * 	Open File Button
+		 */
+	    
+	    openLog = new JButton("Open Log File");
+	    
+	    openLog.setLocation(420, 500);
+	    openLog.setSize(150,150);
+	    openLog.addActionListener(this);
+	    
+	    this.add(openLog);
 		
+	    /*
+		 * 	Log file content
+		 */
+	    
+	    logContent = new JTextArea();
+	    
+	    JScrollPane sp = new JScrollPane(logContent);  
+	    sp.setLocation(20, 500);
+	    sp.setSize(380,150);
+	    
+	    this.add(sp);
+	    
 		this.setResizable(false);
 		this.setVisible(true);
 		
@@ -49,7 +87,48 @@ public class Main extends JFrame implements ActionListener, KeyListener, WindowL
 		JButton bottone;
 		bottone = (JButton) e.getSource();
 		
-		
+		if(bottone == openLog){
+			
+			String logText = new String();
+			
+			/*
+			 *	Creating Log file 
+			 */
+			
+			try {
+				
+				String boxContent = new String();
+				
+				// Running command
+				
+				Process p = Runtime.getRuntime().exec("git log");
+			    InputStream i = p.getInputStream();
+			    InputStreamReader isr = new InputStreamReader(i);
+			    BufferedReader br = new BufferedReader(isr);
+
+			    File f = new File("output.txt");
+			    FileWriter fw = new FileWriter(f);            // for appending use (f,true)
+			    BufferedWriter bw = new BufferedWriter(fw);
+			 
+			    while((br.readLine())!=null){
+			    
+			    	boxContent += br.readLine() + "\n";
+			    	bw.write(br.readLine());
+			    
+			    }
+			    
+			    logContent.setText(boxContent);
+			    
+			    bw.close();
+			    fw.close();
+			    	
+			} catch (IOException e1) {
+			
+				JOptionPane.showMessageDialog(null, e1);
+				
+			}
+			
+		}
 		
 	}
 	
